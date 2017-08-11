2d61c046c58617abb2af56553829ad0e164c11b1
PHU VIET <username@users.noreply.github.com>
2017-08-11 11:14:14 +0700

    angular day09

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FormComponent } from './form.component';
-import { ListComponent } from './list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
-    ListComponent,
   ],
   imports: [
     BrowserModule,
