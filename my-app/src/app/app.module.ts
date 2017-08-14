23b1a5463e7fa1d8647b45d3f886bff6004c1d4e
PHU VIET <username@users.noreply.github.com>
2017-08-14 15:25:33 +0700

    angular day09

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductsService } from './products.service';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component'
+
+const routes: Routes =[
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'products', component: ProductsComponent},
+  { path: 'products/:id', component: ProductDetailComponent},
+  { path: 'cart', component: CartComponent }
+]
+
+export const routing = RouterModule.forRoot(routes)
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductsComponent,
+    CartComponent,
+    HomeComponent,
+    ProductDetailComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule,
+    RouterModule,
+    routing
   ],
-  providers: [],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
